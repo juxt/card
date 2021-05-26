ec4b1fecc7a73b01f65fb216b8a6161a01387d4f
Malcolm Sparks <mal@juxt.pro>
2021-05-27 00:03:36 +0100

    Evolve seed documents

--- deploy-seeds.sh
+++ deploy-seeds.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-site put-json --file seeds/task.edn -u {{base-uri}}/card/cards/task-1
+site put-json --file seeds/checklist-1.edn -u {{base-uri}}/card/cards/checklist-1
+site put-json --file seeds/task-1a.edn -u {{base-uri}}/card/cards/task-1a
+site put-json --file seeds/task-1b.edn -u {{base-uri}}/card/cards/task-1b
+site put-json --file seeds/section-containing-checklist-1.edn -u {{base-uri}}/card/cards/section-containing-checklist-1
