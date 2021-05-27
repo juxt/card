4403aa0690a0354ea65c81a3a6e49ba60fa0cb47
Malcolm Sparks <mal@juxt.pro>
2021-05-27 16:24:57 +0100

    Prefer loop

--- deploy-seeds.sh
+++ deploy-seeds.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-site put-json --file seeds/checklist-1.edn -u {{base-uri}}/card/cards/checklist-1
-site put-json --file seeds/task-1a.edn -u {{base-uri}}/card/cards/task-1a
-site put-json --file seeds/task-1b.edn -u {{base-uri}}/card/cards/task-1b
-site put-json --file seeds/section-containing-checklist-1.edn -u {{base-uri}}/card/cards/section-containing-checklist-1
+for card in $(ls seeds);
+do
+    site put-json --file seeds/$card -u "{{base-uri}}/card/cards/$(basename -s .edn $card)";
+done
