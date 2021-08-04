995869e23c7e1217aa6f0c61c81f92f947da985e
Alex Davis <alx@juxt.pro>
2021-08-04 14:52:48 +0100

    Add tailwind setup
    Run `npm run dev` or `npm run release` and everything else will get run too

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+    purge: [],
+    darkMode: "media",
+    variants: {},
+    plugins: [
+        require('@tailwindcss/forms')
+    ],
+};
