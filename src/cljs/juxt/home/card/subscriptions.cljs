2d741ab412703badc0a96ab0db54cf2599b48860
Alex Davis <alx@juxt.pro>
2021-08-12 16:39:28 +0100

    Use real data for directory

--- src/cljs/juxt/home/card/subscriptions.cljs
+++ src/cljs/juxt/home/card/subscriptions.cljs
@@ -3,7 +3,8 @@
 (ns juxt.home.card.subscriptions
   (:require
    [juxt.home.card.config :as config]
-   [re-frame.core :as rf]))
+   [re-frame.core :as rf]
+   [clojure.string :as str]))
 
 (rf/reg-sub
  ::current-route
@@ -86,30 +87,47 @@
  (fn [db _]
    (:actions db)))
 
+(defn process-db-user
+  [{:keys [user slack]}]
+  (let [{:keys [name email]} user
+        {:juxt.home/keys [slack-profile-pic]} slack]
+    {:name name
+     :imageUrl
+     slack-profile-pic
+     :coverImageUrl
+     "https://images.unsplash.com/photo-1444628838545-ac4016a5418a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80",
+     :about
+     "<p>Tincidunt quam neque in cursus viverra orci, dapibus nec tristique. Nullam ut sit dolor consectetur urna, dui cras nec sed. Cursus risus congue arcu aenean posuere aliquam.</p>
+   <p>Et vivamus lorem pulvinar nascetur non. Pulvinar a sed platea rhoncus ac mauris amet. Urna, sem pretium sit pretium urna, senectus vitae. Scelerisque fermentum, cursus felis dui suspendisse velit pharetra. Augue et duis cursus maecenas eget quam lectus. Accumsan vitae nascetur pharetra rhoncus praesent dictum risus suspendisse.</p>"
+     :fields {:Phone "(555) 123-4567",
+              :Email email
+              :Title "Senior Front-End Developer",
+              :Team "Product Development",
+              :Location "San Francisco",
+              :Sits "Oasis, 4th floor",
+              :Salary "$145,000",
+              :Birthday "June 8, 1990"}}))
+
 (rf/reg-sub
  ::current-user-profile
  (fn [db _]
-   (let [{:keys [name email]} (-> db :people first :user)
-         {:juxt.home/keys [slack-profile-pic]} (-> db :people first :slack)]
-     (def db db)
-     {
-      :name name
-      :imageUrl
-      slack-profile-pic
-      :coverImageUrl
-      "https://images.unsplash.com/photo-1444628838545-ac4016a5418a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80",
-      :about
-      "<p>Tincidunt quam neque in cursus viverra orci, dapibus nec tristique. Nullam ut sit dolor consectetur urna, dui cras nec sed. Cursus risus congue arcu aenean posuere aliquam.</p>
-   <p>Et vivamus lorem pulvinar nascetur non. Pulvinar a sed platea rhoncus ac mauris amet. Urna, sem pretium sit pretium urna, senectus vitae. Scelerisque fermentum, cursus felis dui suspendisse velit pharetra. Augue et duis cursus maecenas eget quam lectus. Accumsan vitae nascetur pharetra rhoncus praesent dictum risus suspendisse.</p>"
-      :fields {
-               :Phone "(555) 123-4567",
-               :Email email
-               :Title "Senior Front-End Developer",
-               :Team "Product Development",
-               :Location "San Francisco",
-               :Sits "Oasis, 4th floor",
-               :Salary "$145,000",
-               :Birthday "June 8, 1990",
-               }
-      })
-   ))
+   (->> db
+        :people
+        first
+        process-db-user)))
+
+(defn last-initial
+  [{:keys [name] :as user}]
+  ;;get first letter of last word in name
+  (let [name-split (str/split name " ")
+        last-initial (first (last name-split))]
+    last-initial))
+
+(rf/reg-sub
+ ::user-directory
+ (fn [db _]
+   (->> db
+        :people
+        (map process-db-user)
+        (sort-by :name)
+        (group-by last-initial))))
