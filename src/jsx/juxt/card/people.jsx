8e7152f7c6d74dfee14174a4d5aef0860bba6571
Johantonelli <joa@juxt.pro>
2021-08-17 09:24:49 +0100

    Remove unwanted people sections

--- src/jsx/juxt/card/people.jsx
+++ src/jsx/juxt/card/people.jsx
@@ -158,13 +158,6 @@ function People({ profile, directory }, ...props) {
                 </div>
               </Transition.Child>
               <div className="flex-1 h-0 pt-5 pb-4 overflow-y-auto">
-                <div className="flex-shrink-0 flex items-center px-4">
-                  <img
-                    className="h-8 w-auto"
-                    src="https://tailwindui.com/img/logos/workflow-logo-pink-500-mark-gray-900-text.svg"
-                    alt="Workflow"
-                  />
-                </div>
                 <nav aria-label="Sidebar" className="mt-5">
                   <div className="px-2 space-y-1">
                     {navigation.map((item) => (
@@ -248,13 +241,6 @@ function People({ profile, directory }, ...props) {
           {/* Sidebar component, swap this element with another sidebar if you like */}
           <div className="flex flex-col h-0 flex-1 border-r border-gray-200 bg-gray-100">
             <div className="flex-1 flex flex-col pt-5 pb-4 overflow-y-auto">
-              <div className="flex items-center flex-shrink-0 px-4">
-                <img
-                  className="h-8 w-auto"
-                  src="https://tailwindui.com/img/logos/workflow-logo-pink-500-mark-gray-900-text.svg"
-                  alt="Workflow"
-                />
-              </div>
               <nav className="mt-5 flex-1" aria-label="Sidebar">
                 <div className="px-2 space-y-1">
                   {navigation.map((item) => (
@@ -471,43 +457,6 @@ function People({ profile, directory }, ...props) {
                     />
                   </div>
                 </dl>
-              </div>
-
-              {/* Team member list */}
-              <div className="mt-8 max-w-5xl mx-auto px-4 pb-12 sm:px-6 lg:px-8">
-                <h2 className="text-sm font-medium text-gray-500">
-                  Team members
-                </h2>
-                <div className="mt-1 grid grid-cols-1 gap-4 sm:grid-cols-2">
-                  {team.map((person) => (
-                    <div
-                      key={person.handle}
-                      className="relative rounded-lg border border-gray-300 bg-white px-6 py-5 shadow-sm flex items-center space-x-3 hover:border-gray-400 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-pink-500"
-                    >
-                      <div className="flex-shrink-0">
-                        <img
-                          className="h-10 w-10 rounded-full"
-                          src={person.imageUrl}
-                          alt=""
-                        />
-                      </div>
-                      <div className="flex-1 min-w-0">
-                        <a href="#" className="focus:outline-none">
-                          <span
-                            className="absolute inset-0"
-                            aria-hidden="true"
-                          />
-                          <p className="text-sm font-medium text-gray-900">
-                            {person.name}
-                          </p>
-                          <p className="text-sm text-gray-500 truncate">
-                            {person.role}
-                          </p>
-                        </a>
-                      </div>
-                    </div>
-                  ))}
-                </div>
               </div>
             </article>
           </main>
