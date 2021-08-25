a40a7100ece91cd29eac1230160c95e9722e522d
Johantonelli <joa@juxt.pro>
2021-08-25 09:39:32 +0100

    Remove workflow logo from sidebar

--- src/js/stories/DesktopSidebar.tsx
+++ src/js/stories/DesktopSidebar.tsx
@@ -6,13 +6,6 @@ export function Sidebar({ user, ...props }: SidebarProps) {
   return (
     <div className="flex-1 flex flex-col min-h-0 border-r border-gray-200 bg-white max-w-sm">
       <div className="flex-1 flex flex-col pt-5 pb-4 overflow-y-auto">
-        <div className="flex items-center flex-shrink-0 px-4">
-          <img
-            className="h-8 w-auto"
-            src="https://tailwindui.com/img/logos/workflow-logo-indigo-600-mark-gray-800-text.svg"
-            alt="Card"
-          />
-        </div>
         <SidebarNav {...props} />
       </div>
       <SidebarFooter user={user}></SidebarFooter>
