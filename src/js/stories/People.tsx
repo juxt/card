64f1b2ada8643d3e0f99f16954da796c632d7a5b
Alex Davis <alx@juxt.pro>
2021-10-07 19:30:17 +0100

    Remove some unused view code and rejig the people page/sidebar

--- src/js/stories/People.tsx
+++ src/js/stories/People.tsx
@@ -1,38 +1,13 @@
 import { useState } from "react";
-import {
-  CalendarIcon,
-  CogIcon,
-  HomeIcon,
-  MapIcon,
-  MenuIcon,
-  SearchCircleIcon,
-  SpeakerphoneIcon,
-  UserGroupIcon,
-  ViewGridAddIcon,
-} from "@heroicons/react/outline";
-import { MobileSidebar } from "./MobileSidebar";
-import { Sidebar } from "./DesktopSidebar";
+import { ChevronLeftIcon } from "@heroicons/react/outline";
 import { ProfileHeader } from "./ProfileHeader";
 import { Tabs } from "./Tabs";
 import { DescriptionList } from "./DescriptionList";
 import { DirectoryList } from "./DirectoryList";
 import { PeopleProps } from "../types";
-import Breadcrumb from "./Breadcrumb";
 import { EventCalendar } from "./Calendar";
-
-const navigation = [
-  { name: "Dashboard", path: "#", icon: HomeIcon, current: false },
-  { name: "Calendar", path: "#", icon: CalendarIcon, current: false },
-  { name: "Teams", path: "#", icon: UserGroupIcon, current: false },
-  { name: "Directory", path: "#", icon: SearchCircleIcon, current: true },
-  { name: "Announcements", path: "#", icon: SpeakerphoneIcon, current: false },
-  { name: "Office Map", path: "#", icon: MapIcon, current: false },
-];
-
-const secondaryNavigation = [
-  { name: "Apps", path: "#", icon: ViewGridAddIcon },
-  { name: "Settings", path: "#", icon: CogIcon },
-];
+import { Disclosure, Transition } from "@headlessui/react";
+import { useWindowSize } from "../hooks/useWindowSize";
 
 const tabs = [
   { name: "Profile", path: "#", current: true },
@@ -48,79 +23,83 @@ export function People({
   onUpdateEvent,
   onDeleteEvent,
 }: PeopleProps) {
-  const [sidebarOpen, setSidebarOpen] = useState(false);
   const [selectedTab, setSelectedTab] = useState(tabs[0].name);
-  const fakeProps = {
-    navigation: navigation,
-    secondaryNavigation: secondaryNavigation,
-    tabs: tabs,
-  };
+  const size = useWindowSize();
+  const isMobile = size?.width && size.width < 768;
   return (
     <div className="relative h-screen flex overflow-hidden bg-white">
-      <MobileSidebar
-        {...fakeProps}
-        user={user}
-        sidebarOpen={sidebarOpen}
-        setSidebarOpen={setSidebarOpen}
-      />
-      {/* Static sidebar for desktop */}
-      <div className="hidden lg:flex lg:flex-shrink-0">
-        <div className="flex flex-col w-64">
-          <Sidebar {...fakeProps} user={user} />
-        </div>
-      </div>
-
-      <div className="flex flex-col min-w-0 flex-1 overflow-hidden">
-        <div className="lg:hidden">
-          <div className="flex items-center justify-between bg-gray-50 border-b border-gray-200 px-4 py-1.5">
-            <div>
-              <button
-                type="button"
-                className="-mr-3 h-12 w-12 inline-flex items-center justify-center rounded-md text-gray-500 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-pink-600"
-                onClick={() => setSidebarOpen(true)}
-              >
-                <span className="sr-only">Open sidebar</span>
-                <MenuIcon className="h-6 w-6" aria-hidden="true" />
-              </button>
-            </div>
-          </div>
-        </div>
-        <div className="flex-1 relative z-0 flex overflow-hidden">
-          <main className="flex-1 relative z-0 overflow-y-auto focus:outline-none xl:order-last">
-            <Breadcrumb page={navigation.find((el) => el.current)!} />
-
-            <article>
-              <ProfileHeader
-                profile={profile}
-                isLoading={isProfileLoading}
-              ></ProfileHeader>
-              <Tabs
-                tabs={tabs}
-                selectedTab={selectedTab}
-                setSelectedTab={setSelectedTab}
-              />
-              {selectedTab === "Profile" && (
-                <>
+      <Disclosure
+        as="div"
+        defaultOpen
+        className="flex-1 relative z-0 flex overflow-hidden"
+      >
+        {({ open, close }) => (
+          <>
+            <Transition
+              show={open}
+              enter="transition ease-out duration-200 transform"
+              enterFrom="-translate-x-40"
+              enterTo="translate-x-0"
+              leave="transition ease-in duration-200 transform"
+              leaveFrom="translate-x-0"
+              leaveTo="-translate-x-40"
+            >
+              <Disclosure.Panel
+                as={DirectoryList}
+                static
+                directory={directory}
+                isLoading={isDirectoryLoading}
+              ></Disclosure.Panel>
+            </Transition>
+            <main
+              onClick={() => isMobile && open && close()}
+              style={{ zIndex: -1 }}
+              className="flex-1 relative overflow-y-auto focus:outline-none xl:order-last"
+            >
+              {!open && (
+                <Disclosure.Button
+                  className="flex items-start px-4 py-3 sm:px-6 md:px-8"
+                  aria-label="Open directory list"
+                >
+                  <ChevronLeftIcon
+                    className="-ml-2 h-5 w-5 text-gray-400"
+                    aria-hidden="true"
+                  />
+                  <span className="inline-flex items-center space-x-3 text-sm font-medium text-gray-900">
+                    Show directory
+                  </span>
+                </Disclosure.Button>
+              )}
+              <article>
+                <ProfileHeader
+                  profile={profile}
+                  isLoading={isProfileLoading}
+                ></ProfileHeader>
+                <Tabs
+                  tabs={tabs}
+                  selectedTab={selectedTab}
+                  setSelectedTab={setSelectedTab}
+                />
+                {selectedTab === "Profile" && (
                   <DescriptionList
                     profile={profile}
                     isLoading={isProfileLoading}
-                  ></DescriptionList>
-                </>
-              )}
-              {selectedTab === "Calendar" && (
-                <EventCalendar
-                  events={profile?.holidays || []}
-                  projectOptions={profile?.projects || []}
-                  isCurrentUser={user?.id === profile?.id}
-                  onDeleteEvent={onDeleteEvent}
-                  onUpdateEvent={onUpdateEvent}
-                />
-              )}
-            </article>
-          </main>
-          <DirectoryList directory={directory} isLoading={isDirectoryLoading} />
-        </div>
-      </div>
+                  />
+                )}
+                {selectedTab === "Calendar" && (
+                  <EventCalendar
+                    events={profile?.holidays || []}
+                    projectOptions={profile?.projects || []}
+                    isCurrentUser={user?.id === profile?.id}
+                    onDeleteEvent={onDeleteEvent}
+                    onUpdateEvent={onUpdateEvent}
+                  />
+                )}
+              </article>
+            </main>
+          </>
+        )}
+      </Disclosure>
     </div>
   );
 }
